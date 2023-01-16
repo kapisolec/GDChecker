fd9fa6079234478478edcd5c7ba3e10aa8c89818
Frashier <kamil@DESKTOP-C289Q2C.localdomain>
2023-01-16 21:14:05 +0100

    revert to recursion

--- src/EthersHandler.ts
+++ src/EthersHandler.ts
@@ -1,9 +1,6 @@
 import { ethers } from "ethers"
 import serverLog from "./utils/serverLog"
 import MongoHandler from "./MongoHandler"
-import { throws } from "assert";
-import { cursorTo } from "readline";
-import { sign } from "crypto";
 
 const maxArraySize = 100;
 const blacklist = [
@@ -28,10 +25,6 @@ export default class EthersHandler {
     uint32: 1,
     uint16: 1,
     uint8: 1,
-    bytes32: "0x6d6168616d000000000000000000000000000000000000000000000000000000",
-    bytes24: "",
-    bytes4: "",
-    bytes1: "",
     bool: false,
     int256: 1
   }
@@ -42,100 +35,104 @@ export default class EthersHandler {
     this.mockValues.bytes = ethers.utils.randomBytes(1);
   }
 
-  private generateValues(inputs: ethers.utils.ParamType[]): [] {
-    const values: any = [];
-    const stack = [...inputs]
-    // console.log(inputs)
-    while (stack.length > 0) {
-      const input = stack.shift() as ethers.utils.ParamType;
-      const inputType = input.type.replace(/\d/g, '');
+  private generateValues(inputs: ethers.utils.ParamType[]): any[] {
+    let values: any[] = [];
+    for (const input of inputs) {
+      //console.log(input)
       let value;
 
-      if (inputType === 'bytes') {
+      const inputTypeSimplified = input.type.replace(/\d/g, '');
+      if (inputTypeSimplified === 'bytes') {
         const typeLength = /\d+/.exec(input.type) === null ? '1' : /\d+/.exec(input.type)![0]
         value = ethers.utils.randomBytes(parseInt(typeLength))
       } else {
-        value = this.mockValues[inputType]
+        value = this.mockValues[inputTypeSimplified]
       }
 
       if (value === undefined) {
         if (input.arrayLength !== null && maxArraySize > input.arrayLength) {
           const iterations = input.arrayLength === -1 ? 1 : input.arrayLength;
-          stack.push(...Array(iterations).fill(input.arrayChildren))
+          values.push([...this.generateValues(Array(iterations).fill(input.arrayChildren))])
           continue;
         }
         if (input.components !== null) {
-          stack.push(...input.components)
+          values.push([...this.generateValues(input.components)])
           continue;
         }
         // serverLog(`Unsupported type: ${input.type}`)
       } else {
-        values.push(value);
+        values.push(value)
       }
     }
 
-    // console.log(values)
     return values;
   }
+
+  // private generateValues(inputs: ethers.utils.ParamType[]): [] {
+  //   let values: string = ""
+  //   const stack = [...inputs]
+  //   const nestStack: number[] = []
+
+  //   while (stack.length > 0) {
+  //     if (values !== "") {
+  //       values += ','
+  //     }
+
+  //     const input = stack.shift() as ethers.utils.ParamType;
+  //     const inputType = input.type.replace(/\d/g, '');
+  //     let value;
+
+  //     if (inputType === 'bytes') {
+  //       const typeLength = /\d+/.exec(input.type) === null ? '1' : /\d+/.exec(input.type)![0]
+  //       value = ethers.utils.randomBytes(parseInt(typeLength))
+  //     } else {
+  //       value = this.mockValues[inputType]
+  //     }
+
+  //     if (value === undefined) {
+  //       if (input.arrayLength !== null && maxArraySize > input.arrayLength) {
+  //         const iterations = input.arrayLength === -1 ? 1 : input.arrayLength;
+  //         nestStack.unshift(iterations)
+  //         stack.unshift(...Array(iterations).fill(input.arrayChildren))
+  //         continue;
+  //       }
+  //       if (input.components !== null) {
+  //         stack.unshift(...input.components)
+  //         continue;
+  //       }
+  //       // serverLog(`Unsupported type: ${input.type}`)
+  //     } else {
+  //       values += value;
+  //     }
+  //   }
+
+  //   return JSON.parse(values);
+  // }
 
   async simulateSignatures(address: string): Promise<string[]> {
     const usedFunctions: string[] = [];
     const signatureCursor = await this.mongoHandler.collection.find();
 
     while (await signatureCursor.hasNext()) {
-      // console.time("iteration")
-      console.time('dupa')
       const signature = await signatureCursor.next();
       const abi = `function ${signature.text_signature}`;
       const fragment = ethers.utils.Fragment.from(abi)
       const contract = new ethers.Contract(address, [abi], this.provider)
+      console.log(signature.text_signature)
       const values = this.generateValues(fragment.inputs);
-      console.timeEnd('dupa')
 
-      console.time('cyce')
       try {
         await contract.callStatic[fragment.name](...values)
         serverLog(`${signature.hex_signature} - ${signature.text_signature} found for ${address}`)
-        // console.log(values)
-        usedFunctions.push(signature.text_signature)
+        usedFunctions.push(`${signature.hex_signature}: ${signature.text_signature}`)
       } catch (e) {
-        if ((e as any).code === ethers.utils.Logger.errors.NETWORK_ERROR) {
-          console.log(signature.text_signature)
-          console.log(values)
-          console.log(e)
-        }
+        // if ((e as any).code !== ethers.utils.Logger.errors.CALL_EXCEPTION) {
+        //   console.log(signature.text_signature)
+        //   console.log(values)
+        //   console.log(e)
+        // }
       }
-      console.timeEnd('cyce')
-
-      // console.timeEnd("iteration")
     }
-    //   const abi = `function ${signature.text_signature}`;
-    //   const fragment = ethers.utils.Fragment.from(abi)
-    //   const contract = new ethers.Contract(address, [abi], this.provider)
-    //   console.log(signature.text_signature)
-    //   const values = this.generateValues(fragment.inputs);
-    // console.log(values)
-    // if (fragment.inputs.some(input => {
-    //   input.baseType === 'tuple' || input.baseType === 'array'
-    // })) {
-    //   console.log(abi)
-    //   console.log(values)
-    // }
-
-    //   continue;
-    //   try {
-    // await contract.callStatic[fragment.name](...values)
-    //     serverLog(`${signature.hex_signature} - ${signature.text_signature} found for ${address}`)
-    //     usedFunctions.push(signature.text_signature)
-    //   } catch (e) {
-    //     if ((e as any).code !== ethers.utils.Logger.errors.NETWORK_ERROR) {
-    //       console.log(signature.text_signature)
-    //       console.log(result)
-    //       console.log(e)
-    //     }
-    //     continue
-    //   }
-    // }
     return usedFunctions
   }
 }
